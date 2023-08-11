f5753f1854767c6437bf402d91a6924136785bbe
nuevo user <user@gmail.com>
2023-08-11 14:20:12 +0200

    2da version

--- app.py
+++ app.py
@@ -1,63 +1,79 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from model import Cliente
 from pathlib import Path
-
-#app = Flask(__name__, static_url_path='/static', static_folder='fontello-ec2f3e69')
-
 
 app = Flask(__name__)
 
 # Cargar datos de clientes desde un archivo CSV
 Cliente.cargar_datos(Path('data') / 'clientes.csv')
 
-""" #Ruta para servir archivos estáticos de Fontello
-@app.route('/static/<path:path>')
-def send_fontello(path):
-    return send_from_directory('fontello-ec2f3e69', path) """
-
 @app.route('/')
 def index():
-    # Obtener parámetros de búsqueda de la URL
     search_query = request.args.get('search', '').lower()
     _page = int(request.args.get('_page', 1))
 
-    # Filtrar clientes según la consulta de búsqueda o obtener todos los clientes
     if search_query:
         clientes = [cliente for cliente in Cliente.todos() if search_query in cliente.nombre.lower()]
     else:
         clientes = list(Cliente.todos())
 
-    # Define el tamaño de página deseado
     PAGE_SIZE = 15
-    
-    # Calcular la paginación
+
     num_clientes = len(clientes)
     num_pages = (num_clientes + PAGE_SIZE - 1) // PAGE_SIZE
     clientes_paginados = clientes[(_page - 1) * PAGE_SIZE:_page * PAGE_SIZE]
 
-    # Calcular las páginas para la paginación
     start_page = max(1, (_page - 1) // 3 * 3 + 1)
     end_page = min(num_pages, (_page - 1) // 3 * 3 + 4)
     pages = range(start_page, end_page)
 
-    # Renderizar la plantilla 'clientes.html' con los datos necesarios
     return render_template('clientes.html', clientes=clientes_paginados, pages=pages, _page=_page, num_pages=num_pages, search_query=search_query)
-
-
 
 @app.route('/clientes/<int:id>')
 def detalle_cliente(id):
-    # Cargar un cliente específico desde el modelo
     cliente = Cliente.buscar(id)
-
-    # Renderizar la plantilla 'detalle_cliente.html' con los datos del cliente
     return render_template('detalle_cliente.html', cliente=cliente)
 
-#Ruta para eliminar un cliente por ID
-@app.route('eliminar_cliente/<int:id>')
-def eliminar_cliente(id):
-    return redirect(url_for('clientes'))
+@app.route('/editar_cliente/<int:id>', methods=['GET', 'POST'])
+def editar_cliente(id):
+    cliente = Cliente.buscar(id)
+    if cliente:
+        if request.method == 'POST':
+            nombre = request.form['nombre']
+            apellidos = request.form['apellidos']
+            # Obtiene más datos del formulario y actualiza el cliente
+            Cliente.editar(id, nombre=nombre, apellidos=apellidos)
+            return redirect(url_for('index'))
+        return render_template('editar_cliente.html', cliente=cliente)
+    else:
+        return "Cliente no encontrado"  # Puedes personalizar este mensaje según tu necesidad
 
+@app.route('/nuevo_cliente', methods=['GET', 'POST'])
+def nuevo_cliente():
+    if request.method == 'GET':
+        return render_template('nuevo_cliente.html')  # Mostrar el formulario de creación
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellidos = request.form['apellidos']
+        sexo = request.form['sexo']
+        email = request.form['email']
+        telefono = request.form['telefono']
+        direccion = request.form['direccion']
+        ciudad = request.form['ciudad']
+        pais = request.form['pais']
+        
+        # Llama a la función nuevo en la clase Cliente para crear un nuevo cliente
+        Cliente.nuevo(nombre=nombre, apellidos=apellidos, sexo=sexo, email=email,
+                      telefono=telefono, direccion=direccion, ciudad=ciudad, pais=pais)
+        
+        return redirect(url_for('index'))  # Redirigir a la página de inicio """
+        
+@app.route('/eliminar_cliente/<int:id>')
+def eliminar_cliente(id):
+    if Cliente.eliminar(id):
+        return redirect(url_for('index'))
+    else:
+        return "Cliente no encontrado"  # Puedes personalizar este mensaje según tu necesidad
 
 
 if __name__ == '__main__':
